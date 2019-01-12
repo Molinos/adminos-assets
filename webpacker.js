97316548f5e91ef63b3b5d75e957681536b746c5
Alex Semyonov <alex@semyonov.us>
2019-01-12 03:44:58 +0300

    Disable CommonChunkVendor that breaks everything

--- webpacker.js
+++ webpacker.js
@@ -26,12 +26,23 @@ function setupAdminos(environment) {
     "window.jQuery": "jquery",
   }));
 
-  // TODO: replace with SplitChunks after upgrade to Webpack 4.x
-  environment.plugins.insert("CommonChunkVendor",
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      minChunks: (module) => module.context && module.context.indexOf("node_modules") !== -1
-    }),
-    { before: "manifest" }
-  );
+  // // TODO: replace with SplitChunks after upgrade to Webpack 4.x
+  // // TODO: also do we really need it?
+  // environment.plugins.append("CommonChunkVendor",
+  //   new webpack.optimize.CommonsChunkPlugin({
+  //     name: "vendor",
+  //     minChunks: (module) => {
+  //       // this assumes your vendor imports exist in the node_modules directory
+  //       return module.context && module.context.indexOf('node_modules') !== -1
+  //     }
+  //   })
+  // );
+  //
+  // environment.plugins.append(
+  //   'CommonsChunkManifest',
+  //   new webpack.optimize.CommonsChunkPlugin({
+  //     name: 'manifest',
+  //     minChunks: Infinity
+  //   })
+  // )
 }
